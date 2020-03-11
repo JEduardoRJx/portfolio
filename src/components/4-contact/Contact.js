6840852d02558176e446d0c1dfdea70a2e2de443
Eduardo <jeduardorjx@gmail.com>
2020-03-11 12:58:27 -0600

    add imports

---
+++ src/components/4-contact/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Contact.scss';
+import Fade from 'react-reveal/Fade';
+import ContactForm from '../4-contactform/ContactForm';
+import Github from '../../images/github.png';
+import Linkedin from '../../images/linkedin.png';
+import Resume from '../../images/resume.png'
