498bca4de7eabdeff5d3a4931a9a731d30acc234
Eduardo <jeduardorjx@gmail.com>
2020-03-11 13:00:16 -0600

    Set contact section structure

--- src/components/4-contact/Contact.js
+++ src/components/4-contact/Contact.js
@@ -5,3 +5,14 @@ import ContactForm from '../4-contactform/ContactForm';
 import Github from '../../images/github.png';
 import Linkedin from '../../images/linkedin.png';
 import Resume from '../../images/resume.png'
+
+const Contact = () => {
+  return (
+    <section className='contact-section'>
+      
+
+    </section>
+  );
+}
+
+export default Contact;
