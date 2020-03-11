18d54f1cbc253803b36aad9a74275bf4274f5150
Eduardo <jeduardorjx@gmail.com>
2020-03-11 11:38:13 -0600

    Set component structure

---
+++ src/components/1-mtnimg/MtnImg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MtnImg.scss';
+import Fade from 'react-reveal/Fade';
+
+const MtnImg = () => {
+  return (
+    <Fade bottom>
+      <section className='mtnimg-section'></section>
+    </Fade>
+  );
+}
+
+export default MtnImg;
