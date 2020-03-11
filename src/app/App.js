cf784fc6ab2654d8dbd684031ab7058a3af3cd81
Eduardo <jeduardorjx@gmail.com>
2020-03-11 12:21:25 -0600

    Import aboutme component

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,8 @@ import Bkgrnd from '../components/bkgrnd/Bkgrnd';
 import MtnImg from '../components/1-mtnimg/MtnImg';
 import Name from '../components/1-name/Name';
 import Nav from '../components/1-nav/Nav';
+import AboutMe from '../components/2-aboutme/AboutMe';
+
 
 
 
@@ -16,6 +18,7 @@ function App() {
       <Name />
       <MtnImg />
       <Bkgrnd section='mtnbkgrnd-section' />
+      <AboutMe />
       <Bkgrnd section='aboutmebkgrnd-section' />
       <Bkgrnd section='portfoliobkgrnd-section' />
       <Bkgrnd section='contactbkgrnd-section' />
