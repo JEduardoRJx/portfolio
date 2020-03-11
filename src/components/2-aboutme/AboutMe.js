2eff0a331b7c7afd3dc0b4196adecd345b798866
Eduardo <jeduardorjx@gmail.com>
2020-03-11 12:17:46 -0600

    Add about me text

--- src/components/2-aboutme/AboutMe.js
+++ src/components/2-aboutme/AboutMe.js
@@ -12,6 +12,12 @@ const AboutMe = () => {
             src={Me}
             alt='J. Eduardo headshot' />
         </div>
+
+        <div className='text-wrapper'>
+          <p className='text'>
+            Originally from Mexico, I’m a Software Engineer with a passion to create, design, and build eye-catching applications. My background helping immigrants and other under-served populations during my time at Denver Public Library made me realize we can create friendlier, more accessible, and easier to use applications. I graduated from Turing School of Software and Design with an emphasis on Front-End Engineering and I strive to make a positive impact by solving challenging problems.
+          </p>
+        </div>
     </Fade>
       </section>
   );
