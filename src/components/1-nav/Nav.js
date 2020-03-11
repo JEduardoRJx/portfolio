89f924f02f24c899081166a75bf9ffd5c8cb773c
Eduardo <jeduardorjx@gmail.com>
2020-03-11 11:58:08 -0600

    Add structure for nav component

--- src/components/1-nav/Nav.js
+++ src/components/1-nav/Nav.js
@@ -4,3 +4,41 @@ import { Link } from 'react-scroll';
 import Fade from 'react-reveal/Fade';
 import Menu from '../../images/menu.svg';
 import Close from '../../images/close.svg';
+
+
+const Nav = () => {
+
+  return (
+    <>
+      <Fade top cascade delay={500}>
+        <nav className='nav-desktop'>
+          <Link activeClass='active'
+            className='link-nav'
+            to='aboutmebkgrnd-section'
+            smooth={true}
+            duration={500}>
+              About Me
+          </Link>
+
+          <Link activeClass='active'
+            className='link-nav'
+            to='portfoliobkgrnd-section'
+            smooth={true}
+            duration={500}>
+              Portfolio
+          </Link>
+
+          <Link activeClass='active'
+            className='link-nav'
+            to='contactbkgrnd-section'
+            smooth={true}
+            duration={500}>
+              Contact
+          </Link>
+        </nav>
+      </Fade>
+    </>
+  );
+}
+
+export default Nav;
