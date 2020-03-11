1af94eff0e719f60e663bde9c3568ee8ea7fd954
Eduardo <jeduardorjx@gmail.com>
2020-03-11 11:07:22 -0600

    Set directory for App

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+// import Name from '../';
+// import Nav from '../components/1-nav/Nav';
+// import MtnImg from '../components/1-mtnimg/MtnImg';
+// import AboutMe from '../components/2-aboutme/AboutMe';
+// import Portfolio from '../components/3-portfolio/Portfolio';
+// import Contact from '../components/4-contact/Contact';
+// import Bkgrnd from '../components/bkgrnd/Bkgrnd';
+
+function App() {
+  return (
+    <>
+    hello
+    </>
+  );
+}
+
+export default App;
