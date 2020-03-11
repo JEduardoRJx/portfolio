b1a02abbe544e23c0a4fa9c118da7ee98bb2311c
Eduardo <jeduardorjx@gmail.com>
2020-03-11 12:17:25 -0600

    Set component structure

--- src/components/2-aboutme/AboutMe.js
+++ src/components/2-aboutme/AboutMe.js
@@ -2,3 +2,19 @@ import React from 'react';
 import './AboutMe.scss';
 import Fade from 'react-reveal/Fade';
 import Headshot from '../../images/headshot.jpg';
+
+const AboutMe = () => {
+  return (
+    <section className='aboutme-section'>
+      <Fade bottom delay={100}>
+        <div className='headshot-wrapper'>
+          <img className='headshot'
+            src={Me}
+            alt='J. Eduardo headshot' />
+        </div>
+    </Fade>
+      </section>
+  );
+}
+
+export default AboutMe;
