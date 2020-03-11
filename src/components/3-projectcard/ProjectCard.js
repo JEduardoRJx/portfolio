91af6f47d1ae98efd3cbdf397e4402961cf7a45c
Eduardo <jeduardorjx@gmail.com>
2020-03-11 12:32:43 -0600

    Add in needed imports for project card

---
+++ src/components/3-projectcard/ProjectCard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './ProjectCard.scss';
+import Github from '../../images/github_black.png';
+import Deploy from '../../images/deploy.png';
+import Fade from 'react-reveal/Fade';
