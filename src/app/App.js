06add61ac7fad0f7837a23a9701daaaee95757ff
Eduardo <jeduardorjx@gmail.com>
2020-03-11 11:42:30 -0600

    Import the MtnImg component into app

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Bkgrnd from '../components/bkgrnd/Bkgrnd';
+import MtnImg from '../components/1-mtnimg/MtnImg';
+
 
 
 function App() {
   return (
     <>
+      <MtnImg />
       <Bkgrnd section='mtnbkgrnd-section' />
       <Bkgrnd section='aboutmebkgrnd-section' />
       <Bkgrnd section='portfoliobkgrnd-section' />
