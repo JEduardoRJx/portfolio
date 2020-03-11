1d35292bdc475459ac8c47873cf805f31b29f4ac
Eduardo <jeduardorjx@gmail.com>
2020-03-11 11:56:50 -0600

    Add all imports

---
+++ src/components/1-nav/Nav.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import './Nav.scss';
+import { Link } from 'react-scroll';
+import Fade from 'react-reveal/Fade';
+import Menu from '../../images/menu.svg';
+import Close from '../../images/close.svg';
