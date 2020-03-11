bd75e1bd4d6c7bec1ed999b2ccd06ccdaff2edee
Eduardo <jeduardorjx@gmail.com>
2020-03-11 14:09:04 -0600

    Updates

---
+++ precache-manifest.f7d9e81d244bfdadd0d8c4449e39e593.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673f7575d74662096c73dddd38d90d13",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "32c20f18cab1a66812bd",
+    "url": "/portfolio/static/css/main.13cc3f3e.chunk.css"
+  },
+  {
+    "revision": "5465582ecd518dafa55f",
+    "url": "/portfolio/static/js/2.5a2a6662.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.5a2a6662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c20f18cab1a66812bd",
+    "url": "/portfolio/static/js/main.f5acc80a.chunk.js"
+  },
+  {
+    "revision": "809b7b13d36e64caae69",
+    "url": "/portfolio/static/js/runtime-main.c2eba726.js"
+  },
+  {
+    "revision": "27afe9dfbf45297facc3669a340abfae",
+    "url": "/portfolio/static/media/checkyoself.27afe9df.png"
+  },
+  {
+    "revision": "a71727d72456139395ae837e8ba7dfdd",
+    "url": "/portfolio/static/media/close.a71727d7.svg"
+  },
+  {
+    "revision": "d818664f4fec0505aac2cab468c491d6",
+    "url": "/portfolio/static/media/headshot.d818664f.jpg"
+  },
+  {
+    "revision": "8a5985a0bf14fb536d8f5dd92fc28048",
+    "url": "/portfolio/static/media/hp.8a5985a0.png"
+  },
+  {
+    "revision": "fc3f4b8f03777e6eae56d4bcde4dcfcc",
+    "url": "/portfolio/static/media/menu.fc3f4b8f.svg"
+  },
+  {
+    "revision": "4af182e8d8fb550de3cc012767e984e5",
+    "url": "/portfolio/static/media/mountain.4af182e8.png"
+  },
+  {
+    "revision": "b868a7dd51c0776bc66ae3d4bf908ff7",
+    "url": "/portfolio/static/media/movie_tracker.b868a7dd.png"
+  },
+  {
+    "revision": "a6e54c5a4fe7317912e0ee93846e7cbb",
+    "url": "/portfolio/static/media/nightsky.a6e54c5a.jpg"
+  },
+  {
+    "revision": "24744dc4d2b0fd0594580e3fa90a639d",
+    "url": "/portfolio/static/media/whatsnew.24744dc4.png"
+  }
+]);
