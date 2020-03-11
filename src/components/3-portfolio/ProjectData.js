1fa1968c83b4b8bd00dcb498a6945c6b454aeffc
Eduardo <jeduardorjx@gmail.com>
2020-03-11 12:26:51 -0600

    export project data

---
+++ src/components/3-portfolio/ProjectData.js
@@ -0,0 +1,38 @@
+export const Projects = [
+  {
+    id: 1,
+    title: 'Harry Potter & the Sorting Game',
+    img: require('../../images/hp.png'),
+    desc: 'Game to match witches and wizards to their respective Hogwarts house.',
+    repo: 'http://tinyurl.com/hp-repo',
+    deploy: 'http://tinyurl.com/hp-tsg',
+    techStack: ['React', 'JavaScript', 'Redux', 'InVision Studio', 'HP API', 'Router', 'Jest & Enzyme']
+  },
+  {
+    id: 2,
+    title: 'Movie Tracker',
+    img: require('../../images/movie_tracker.png'),
+    desc: 'Movie Tracker is a web application that allows users to view the latest Holywood movies via the The Movie DB API',
+    repo: 'https://tinyurl.com/movie-tracker-repo',
+    deploy: 'https://tinyurl.com/movie-tracker',
+    techStack: ['React', 'JavaScript', 'Redux', 'InVision Studio', 'Movies DB API', 'Router', 'Jest & Enzyme']
+  },
+  {
+    id: 3,
+    title: 'What\'s New',
+    img: require('../../images/whatsnew.png'),
+    desc: 'View local, technology, science, health, and entertainment news.',
+    repo: 'https://tinyurl.com/whats-new-repo',
+    deploy: 'https://tinyurl.com/whats-new',
+    techStack: ['React', 'JavaScript', 'Sketch', 'local data', 'Jest & Enzyme', 'CSS']
+  },
+  {
+    id: 4,
+    title: 'Check Yo\' Self',
+    img: require('../../images/checkyoself.png'),
+    desc: 'A simple Checklist app for desktop and mobile. Created with semantic HTML, organized CSS, and DRY JavaScript.',
+    repo: 'https://tinyurl.com/checkyo-self-repo',
+    deploy: 'https://tinyurl.com/checkyo-self',
+    techStack: ['Vanilla JavaScript', 'HMTL', 'CSS', 'Local Storage']
+  }
+]
